8a9ba98598af77e461b1fe5c1e7d9317b96a39de
jacekwozniak12 <jac3kw0zniak@gmail.com>
2020-01-16 15:35:04 +0100

    testing NAudio_WaveGeneration

--- ChordGenerator/MainWindow.xaml.cs
+++ ChordGenerator/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -12,6 +13,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using NAudio;
+using NAudio.Wave;
+using NAudio.Wave.SampleProviders;
 
 namespace ChordGenerator
 {
@@ -27,7 +31,25 @@ namespace ChordGenerator
 
         private void GenerateButton_Click(object sender, RoutedEventArgs e)
         {
+            var sine20Seconds = new SignalGenerator()
+            {
+                Gain = 0.2,
+                Frequency = 42,
+                Type = SignalGeneratorType.Sin
+            }
+                .Take(TimeSpan.FromSeconds(1));
+            using (var wo = new WaveOutEvent())
+            {
+                wo.Init(sine20Seconds);
+                wo.Play();
+                while (wo.PlaybackState == PlaybackState.Playing)
+                {
+                    Thread.Sleep(500);
+                }
+                wo.Dispose();
+            }
             
+
         }
 
         private void LearnChordsButton_Click(object sender, RoutedEventArgs e)
