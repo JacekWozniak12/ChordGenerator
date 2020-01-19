8f1a76636f2b8be37ea44bab1e84e51a953bb5cb
jacekwozniak12 <jac3kw0zniak@gmail.com>
2020-01-19 23:18:35 +0100

    File structure change - content definitions folder

---
+++ ChordGenerator/ContentDefinitions/Chord.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace ChordGenerator
+{
+    /// <summary>
+    /// Chord created by user;
+    /// </summary>
+    public struct Chord
+    {
+        private const int MaximalNotesPerChord = 30;
+
+        public MusicalNote[] musicalNotes { get; private set; }
+
+        public Chord(MusicalNote[] notes)
+        {
+            // TODO
+            if (notes.Length > MaximalNotesPerChord) 
+                throw new ArgumentException
+                    ($"To many notes {notes.Length}\nReduce amount by {notes.Length - MaximalNotesPerChord}");
+
+
+            musicalNotes =                  notes;
+            defaultPlayType =               PlayType.AllATSameTime;
+            defaultTimeToPlaySingleNote =   0.33f;
+        }
+
+        public enum PlayType
+        {
+            /// <summary>
+            /// Chord notes played one after another
+            /// </summary>
+            Single,
+            /// <summary>
+            /// All chord notes played at the same time
+            /// </summary>
+            AllATSameTime
+        }
+
+        /// <summary>
+        /// How will chord be played.
+        /// </summary>
+        private static PlayType defaultPlayType;
+        /// <summary>
+        /// In seconds
+        /// </summary>
+        private static float defaultTimeToPlaySingleNote;
+    }
+}
---
+++ ChordGenerator/ContentDefinitions/Settings.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NAudio.Wave.SampleProviders;
+
+namespace ChordGenerator
+{
+    /// <summary>
+    /// Class holding settings changes
+    /// </summary>
+    public class Settings
+    {
+        public enum Type
+        {
+            Pitch,
+            Volume,
+            SynthType,
+            Delay,
+            DefaultTypeOfPlay
+        }
+
+        /// <summary>
+        /// Opens file stream and search for config file
+        /// </summary>
+        public Settings(string fileAdress)
+        {
+
+        }
+
+        public Settings
+            (
+            float volume,
+            int pitch,
+            Chord.PlayType defaultPlayType,
+            float defaultTimeToPlaySingleNote,
+            float defaultTimeToPlayChord
+            )
+        {
+            this.volume = volume;
+            this.pitch = pitch;
+            this.defaultPlayType = defaultPlayType;
+            this.defaultTimeToPlaySingleNote = defaultTimeToPlaySingleNote;
+            this.defaultTimeToPlayChord = defaultTimeToPlayChord;
+        }
+
+        public float volume { get; private set; } = 0.5f;
+        public int pitch { get; private set; }  = 0;
+        public Chord.PlayType defaultPlayType
+        { get; private set; } = Chord.PlayType.AllATSameTime;
+        public float defaultTimeToPlaySingleNote { get; private set; } = 0.33f;
+        public float defaultTimeToPlayChord { get; private set; } = 2f;
+
+        // read settings from stored file
+    }
+}
