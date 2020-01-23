77011e13bd4a895ae9b9fabc4131ab167e3bc7ee
Nordram <wulkano2@o2.pl>
2020-01-23 20:02:21 +0100

    ErrorView prep

---
+++ ChordGenerator/View/ErrorWindow.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.ComponentModel;
+using System.Windows;
+
+namespace ChordGenerator
+{
+    /// <summary>
+    /// Logika interakcji dla klasy ErrorWindow.xaml
+    /// </summary>
+    public partial class ErrorWindow : Window
+    {
+        public ErrorWindow(string Error)
+        {
+            InitializeComponent();
+            Error_Content.Text = Error;
+        }
+    }
+}
---
+++ ChordGenerator/View/MainWindow.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.ComponentModel;
+using System.Windows;
+
+namespace ChordGenerator { 
+    /// <summary>
+    /// Logika interakcji dla klasy MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private RuntimeManager runtimeManager;
+        private SyntaxReader syntaxReader;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            InitializeApplication();
+            Application.Current.MainWindow.Closing += new CancelEventHandler(MainWindow_Closing);
+        }
+
+        private void InitializeApplication()
+        {
+            runtimeManager =        new RuntimeManager();
+            //display lang window
+            syntaxReader =          new SyntaxReader();
+            ReadInputPrompt();
+        }
+
+        private void MainWindow_Closing(object sender, CancelEventArgs e)
+        {
+            runtimeManager.Dispose();
+        }
+
+        private void Generate_Button_Click(object sender, RoutedEventArgs e)
+        {
+            ReadInputPrompt();
+        }
+
+        private void Play_Button_Click(object sender, RoutedEventArgs e)
+        {
+            runtimeManager.PlaySound();
+        }
+
+        private void SaveSettings_Button_Click(object sender, RoutedEventArgs e)
+        {
+            ErrorWindow errorWindow = new ErrorWindow("test");
+            errorWindow.Show();
+        }
+
+        private void ReadInputPrompt()
+        {
+            try
+            {
+                syntaxReader.ReadInput(InputPrompt.Text.Trim());
+            }
+            catch (ArgumentException err)
+            {
+                // Łukasz, tu napisz mi przechwytywanie błędu
+            }
+            finally
+            {
+            }
+        }
+    }
+}
